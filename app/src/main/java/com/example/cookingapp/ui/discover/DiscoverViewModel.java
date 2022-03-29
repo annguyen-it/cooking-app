5f3b5f1435cee4bee75966dfea8799d9fc2b794e
annguyen <annguyen.it.utc@gmail.com>
2022-03-29 11:14:08 +0700

    Update

--- app/src/main/java/com/example/cookingapp/ui/discover/DiscoverViewModel.java
+++ app/src/main/java/com/example/cookingapp/ui/discover/DiscoverViewModel.java
@@ -1,19 +1,8 @@
 package com.example.cookingapp.ui.discover;
 
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class DiscoverViewModel extends ViewModel {
-
-    private final MutableLiveData<String> mText;
-
     public DiscoverViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
-    }
-
-    public LiveData<String> getText() {
-        return mText;
     }
 }
